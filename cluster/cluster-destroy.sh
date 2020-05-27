01f992bfca6bfa1e3fc458558ad9b45c1d30e6be
Carlos Mendes <cmendesce@gmail.com>
2020-05-27 20:17:37 -0300

    Rename cluster

--- cluster/cluster-destroy.sh
+++ cluster/cluster-destroy.sh
@@ -1,7 +1,7 @@
 set -e
 
-export NAME=cmendes.k8s.local
-export KOPS_STATE_STORE=s3://kops-k8s-clusters
+export NAME=testbed.k8s.local
+export KOPS_STATE_STORE=s3://testbed.k8s.local
 export REGION=us-east-1
 export ZONES=us-east-1a
 
--- cluster/stop-instances.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-IDS=$(aws ec2 describe-instances --output json | jq -r '.Reservations[].Instances | map(.InstanceId) | join(", ")')
-
-for id in $IDS
-do
-  aws ec2 stop-instances --instance-id $id
-done
